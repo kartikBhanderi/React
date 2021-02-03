39fcf9e9ff79e1fb1fe97987869d978c27ba287e
Kartik Bhanderi <18bce024@nirmauni.ac.in>
2021-02-04 00:04:27 +0530

    classless func component

---
+++ hello/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Greet from './components/Greet';
+import { Component } from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <Greet/>  
+    </div>
+  );
+}
+
+export default App;
---
+++ hello/src/components/Greet.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const  Greet = () => <h1> Hello Kartik ! </h1>;
+
+// export const  Greet = () => <h1> Hello Kartik ! </h1>;
+export default Greet
