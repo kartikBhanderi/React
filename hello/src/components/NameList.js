f00643887f7af66a1037ffafd90ca9aa529d458f
Kartik Bhanderi <18bce024@nirmauni.ac.in>
2021-02-09 23:02:04 +0530

    keys while generating list

--- hello/src/components/NameList.js
+++ hello/src/components/NameList.js
@@ -4,19 +4,22 @@ import Person from './Person'
 function NameList() {
     let persons = [
         {
+            id: 1,
             name: 'kartik',
             age: 20
         },
         {
+            id: 2,
             name: 'vraj',
             age: 19
         },
         {
+            id: 3,
             name: 'yash',
             age: 39
         }
     ]
-    const personList = persons.map(person => <Person person={person}></Person>)
+    const personList = persons.map(person => <Person key={person.name} person={person}></Person>)
     return <div>{personList}</div>   
 }
 
--- hello/src/components/Person.js
+++ hello/src/components/Person.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Person({person}) {
     return (
         <div>
-            <h2>I am {person.name}. I am {person.age} years old.</h2>
+            <h2> I am {person.name}. I am {person.age} years old.</h2>
         </div>
     )
 }
