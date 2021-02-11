c81d18e3cf6bfdd7eb80555eddb68cc0118fae47
Kartik Bhanderi <18bce024@nirmauni.ac.in>
2021-02-11 23:00:37 +0530

    comp LC methods

--- hello/src/components/LifeCycleA.js
+++ hello/src/components/LifeCycleA.js
@@ -18,8 +18,29 @@ class LifeCycleA extends Component {
         return null;
     }
 
+    
     componentDidMount() {
         console.log('componentDidMount')
+    }
+    
+    shouldComponentUpdate(){
+        console.log('shouldComponentUpdate')
+        return true
+    }
+
+    getSnapshotBeforeUpdate(prevProps, prevState){
+        console.log('getSnapshotBeforeUpdate')
+        return null
+    }
+
+    componentDidUpdate() {
+        console.log('componentDidUpdate')
+    }
+
+    changeState = () => {
+        this.setState({
+            name: 'yash'
+        })
     }
 
     render() {
@@ -27,7 +48,8 @@ class LifeCycleA extends Component {
         return (
             <div>
             <div>
-                Lifecycle A
+                <div>Lifecycle A</div>
+                <button onClick={this.changeState}>Change state</button>
             </div>
             <LifeCycleB />
             </div>
