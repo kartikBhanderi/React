d8cbe22fa3de259f8fc867b5309f24abdbdf11a3
Kartik Bhanderi <18bce024@nirmauni.ac.in>
2021-05-27 21:58:12 +0530

    Refs

--- hello/src/App.js
+++ hello/src/App.js
@@ -1,32 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-import Greet from './components/Greet'
-import { Component } from 'react';
-import Welcome from './components/Welcome'
-import Hello from './components/Hello'
-import Message from './components/Message'
-import Counter from './components/Counter'
-import FunctionClick from './components/FunctionClick';
-import ClassClick from './components/ClassClick';
-import EventBind from './components/EventBind';
-import ParentComponent from './components/ParentComponent';
-import UserGreeting from './components/UserGreeting';
-import NameList from './components/NameList';
-import Stylesheet from './components/Stylesheet';
-import Inline from './components/Inline';
 import './appStyles.css'
-import styles from './appStyles.module.css'
-import Form from './components/Form';
-import LifeCycleA from './components/LifeCycleA';
-import FragementDemo from './components/FragementDemo';
-import Table from './components/Table';
+import './components/Ref'
+import Ref from './components/Ref';
 
 function App() {
   return (
     <div className="App">
       
-      <Table />
-     {/* <FragementDemo /> */}
+      <Ref /> 
+
     </div>
   );
 }
